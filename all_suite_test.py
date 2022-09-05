a45cec6f1722c0fa9d77f8a9fd9bc801196cf31b
Pavlo Vovcharuk <pavel0337@gmail.com>
2022-09-05 21:54:04 +0200

    fifth_task

---
+++ all_suite_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from unittest.loader import makeSuite
+
+from test_cases.login_to_the_system import TestLoginPage
+from test_cases.framework import Test
+from test_cases.fill_in_form import TestFillInForm
+
+
+def full_suite():
+   test_suite = unittest.TestSuite()
+   test_suite.addTest(makeSuite(TestLoginPage))
+   test_suite.addTest(makeSuite(Test))
+   test_suite.addTest(makeSuite(TestFillInForm))
+   return test_suite
+
+
+if __name__ == '__main__':
+   runner = unittest.TextTestRunner(verbosity=2)
+   runner.run(full_suite())
---
+++ test_cases/fill_in_form.py
@@ -0,0 +1,105 @@
+import os
+import time
+import unittest
+
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium import webdriver
+
+from pages.dashboard import Dashboard
+from pages.login_page import LoginPage
+from pages.add_player_page import AddPlayer
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestFillInForm(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver_service = Service(executable_path=DRIVER_PATH)
+        #self.driver_service = Service(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=self.driver_service)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_case_1(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.type_in_email('user10@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        add_player = AddPlayer(self.driver)
+        add_player.click_on_the_add_player_button()
+        add_player.type_in_name('Name')
+        add_player.type_in_surname('Surname')
+        add_player.type_in_mainPosition('Random')
+        add_player.type_in_age_field('15062000')
+        add_player.click_on_the_add_submit_button()
+        time.sleep(5)
+
+    def test_case_2(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.type_in_email('user10@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        add_player = AddPlayer(self.driver)
+        add_player.click_on_the_add_player_button()
+        add_player.type_in_name('Name')
+        add_player.type_in_surname('Surname')
+        add_player.type_in_mainPosition('Random')
+        add_player.type_in_age_field('15062000')
+        add_player.type_in_email('$%(?/{[')
+        add_player.click_on_the_add_submit_button()
+        time.sleep(5)
+
+    def test_case_3(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.type_in_email('user10@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        add_player = AddPlayer(self.driver)
+        add_player.click_on_the_add_player_button()
+        add_player.click_on_the_add_submit_button()
+        time.sleep(5)
+
+    def test_case_4(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.type_in_email('user10@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        add_player = AddPlayer(self.driver)
+        add_player.click_on_the_add_player_button()
+        add_player.click_on_the_language_button()
+        time.sleep(5)
+
+    def test_case_5(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.type_in_email('user10@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        add_player = AddPlayer(self.driver)
+        add_player.click_on_the_add_player_button()
+        time.sleep(5)
+        add_player.click_on_the_sign_out_button()
+        time.sleep(5)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
